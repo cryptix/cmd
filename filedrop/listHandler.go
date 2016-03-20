7eeccfae808a570761e2f4ea6c343e97a2f71f07
Henry <cryptix@riseup.net>
2016-03-20 12:44:03 +0100

    filedrop: new templating and lets encrypt

--- filedrop/listHandler.go
+++ filedrop/listHandler.go
@@ -1,38 +1,23 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 	"os"
 
-	"github.com/dustin/go-humanize"
-	"github.com/shurcooL/httpfs/html/vfstemplate"
 	"golang.org/x/net/context"
 )
 
-var (
-	tplFuncs = template.FuncMap{
-		"bytes": func(s int64) string { return humanize.Bytes(uint64(s)) },
-	}
-
-	tpl = template.Must(vfstemplate.ParseGlob(assets, template.New("base").Funcs(tplFuncs), "/*.tmpl"))
-)
-
-func jsHandler(ctx context.Context, resp http.ResponseWriter, req *http.Request) error {
+func jsHandler(ctx context.Context, resp http.ResponseWriter, req *http.Request) (interface{}, error) {
 	dir, err := os.Open(*dumpDir)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer dir.Close()
 
 	fileInfos, err := dir.Readdir(-1)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return tpl.Lookup("js.tmpl").Execute(resp, fileInfos)
-}
-
-func nojsHandler(ctx context.Context, resp http.ResponseWriter, req *http.Request) error {
-	return tpl.Lookup("nojs.tmpl").Execute(resp, nil)
+	return fileInfos, nil
 }
--- filedrop/main.go
+++ filedrop/main.go
@@ -10,12 +10,16 @@ package main
 import (
 	"crypto/tls"
 	"flag"
+	"html/template"
 	"log"
 	"net"
 	"net/http"
 	"os"
 
 	"github.com/cryptix/go/http/render"
+	"github.com/cryptix/go/logging"
+	"github.com/dkumor/acmewrapper"
+	"github.com/dustin/go-humanize"
 	"github.com/goji/httpauth"
 	"github.com/rs/xaccess"
 	"github.com/rs/xhandler"
@@ -33,8 +37,9 @@ var (
 	user = flag.String("user", "", "HTTP BasicAuth User")
 	pass = flag.String("pass", "ChangeMe", "HTTP BasicAuth User")
 
-	sslKey = flag.String("key", "", "Key-file for SSL connections")
-	sslCrt = flag.String("crt", "", "Certificate for SSL connections")
+	ssl    = flag.Bool("ssl", false, "enable SSL (lets encrypt)")
+	sslKey = flag.String("key", "key.pem", "Key-file for SSL connections")
+	sslCrt = flag.String("crt", "cert.pem", "Certificate for SSL connections")
 )
 
 func main() {
@@ -44,60 +49,78 @@ func main() {
 
 	hostName, _ := os.Hostname()
 	conf := xlog.Config{
+		Output: xlog.NewConsoleOutput(),
 		Fields: xlog.F{
 			"role": "filedrop",
 			"host": hostName,
 		},
 	}
+	logging.SetupLogging(&conf)
 
 	c.UseC(xlog.NewHandler(conf))
-
-	// Plug the xlog handler's input to Go's default logger
-	log.SetFlags(0)
-	log.SetOutput(xlog.New(conf))
 
 	c.UseC(xlog.RemoteAddrHandler("ip"))
 	c.UseC(xlog.UserAgentHandler("user_agent"))
 	c.UseC(xlog.RefererHandler("referer"))
 	c.UseC(xlog.RequestIDHandler("req_id", "Request-Id"))
 	c.UseC(xaccess.NewHandler())
+	if *user != "" {
+		c.Use(httpauth.SimpleBasicAuth(*user, *pass))
+	}
+
+	ren, err := render.New(assets, "base.tmpl",
+		render.AddTemplates("js.tmpl", "nojs.tmpl", "base.tmpl"),
+		render.FuncMap(template.FuncMap{
+			"bytes": func(s int64) string { return humanize.Bytes(uint64(s)) },
+		}),
+	)
+	checkFatal(err)
+
+	if !production {
+		c.UseC(ren.GetReloader())
+	}
 
 	mux := xmux.New()
-	mux.GET("/js", render.HTML(jsHandler))
-	mux.GET("/nojs", render.HTML(nojsHandler))
+	mux.GET("/", ren.StaticHTML("base.tmpl"))
+	mux.GET("/js", ren.HTML("js.tmpl", jsHandler))
+	mux.GET("/nojs", ren.StaticHTML("nojs.tmpl"))
 	mux.GET("/downloadAll", render.Binary(zipDownloadHandler))
 	mux.POST("/upload", xhandler.HandlerFuncC(uploadHandler))
 
 	mux.Handle("GET", "/assets/*filepath", http.StripPrefix("/assets/", gzip_file_server.New(assets)))
 	mux.Handle("GET", "/drop/*filepath", http.StripPrefix("/drop/", http.FileServer(http.Dir(*dumpDir))))
 
-	if *user != "" {
-		c.Use(httpauth.SimpleBasicAuth(*user, *pass))
-	}
-
-	if *port == "0" && os.Getenv("PORT") != "" {
-		*port = os.Getenv("PORT")
-	}
-
-	l, err := net.Listen("tcp", *host+":"+*port)
-	checkFatal(err)
-
 	var server http.Server
 	server.Handler = c.Handler(mux)
 
-	if *sslKey != "" {
-		tlsCfg := &tls.Config{}
-		if tlsCfg.NextProtos == nil {
-			tlsCfg.NextProtos = []string{"http/1.1"}
-		}
-		var err error
-		tlsCfg.Certificates = make([]tls.Certificate, 1)
-		tlsCfg.Certificates[0], err = tls.LoadX509KeyPair(*sslCrt, *sslKey)
-		checkFatal(err)
+	var l net.Listener
+	if *ssl {
+		w, err := acmewrapper.New(acmewrapper.Config{
+			Domains: []string{*host},
+			Address: ":443",
 
-		l = tls.NewListener(l, tlsCfg)
+			TLSCertFile: *sslCrt,
+			TLSKeyFile:  *sslKey,
+
+			// Let's Encrypt stuff
+			RegistrationFile: "user.reg",
+			PrivateKeyFile:   "user.pem",
+
+			TOSCallback: acmewrapper.TOSAgree,
+		})
+		checkFatal(err)
+		l, err = tls.Listen("tcp", ":443", w.TLSConfig())
+		checkFatal(err)
+		server.Addr = ":443"
+		server.TLSConfig = w.TLSConfig()
 		log.Printf("Serving at https://%s/", l.Addr())
 	} else {
+		if *port == "0" && os.Getenv("PORT") != "" {
+			*port = os.Getenv("PORT")
+		}
+
+		l, err = net.Listen("tcp", *host+":"+*port)
+		checkFatal(err)
 		log.Printf("Serving at http://%s/", l.Addr())
 	}
 
